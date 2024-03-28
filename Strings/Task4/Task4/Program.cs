d54ce7a96cd68b8ad8248e586ef098befaaba85b
NeedCookies <emilkalim895@gmail.com>
2024-03-28 10:26:32 +0300

    Add solution for task 'минимальный сдвиг'

---
+++ Strings/Task4/Task4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static string FindLexicographicallyMinimalShift(string s)
+        {
+            s += s;
+            int n = s.Length;
+
+            List<int> shifts = new List<int>();
+            for (int i = 0; i < n / 2; i++)
+            {
+                shifts.Add(i);
+            }
+
+            shifts.Sort((a, b) =>
+            {
+                int j = a;
+                int k = b;
+                for (int i = 0; i < n / 2; i++)
+                {
+                    if (s[j] != s[k]) return s[j] - s[k];
+                    j++;
+                    k++;
+                }
+                return 0;
+            });
+
+            return s.Substring(shifts[0], n / 2);
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string result = FindLexicographicallyMinimalShift(s);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
