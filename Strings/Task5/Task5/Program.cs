f33482fa6d043c0dea2818432655a5c081567e08
NeedCookies <emilkalim895@gmail.com>
2024-03-31 10:36:09 +0300

    Add solution for task5'Поиск подстроки с погрешностью'

---
+++ Strings/Task5/Task5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sub = Console.ReadLine();
+            string str = Console.ReadLine();
+
+            var ans = findSubs(sub, str);
+            long count = ans.Length;
+            Console.WriteLine(count);
+            foreach (var item in ans)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        private static bool cmp_str(string a, string b)
+        {
+            bool flag = true;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+                    if (flag)
+                        flag = false;
+                    else
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private static long[] findSubs(string sub, string str)
+        {
+            List<long> ans = new List<long>();
+
+
+            for (int i = 0; i < str.Length - sub.Length + 1; i++)
+            {
+                bool fl = cmp_str(sub, str.Substring(i, sub.Length));
+                if (fl)
+                {
+                    ans.Add(i + 1);
+                }
+            }
+            return ans.ToArray();
+        }
+    }
+}
